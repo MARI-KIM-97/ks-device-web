884bae4e5189b6f6973abc2619fe3a847a582859
MARI-KIM-97 <0304mary@naver.com>
2025-04-29 13:32:53 +0900

    Add vite.config.ts for deploy fix)

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+export default defineConfig({
+  plugins: [react()],
+  root: '.', // 루트 디렉토리 설정
+  publicDir: 'public',
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: path.resolve(__dirname, 'public/index.html'), // index.html 위치 명시
+    },
+  },
+});

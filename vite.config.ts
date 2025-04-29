6c7750b95e24f3002edfe413445485ae9ffaaaa3
MARI-KIM-97 <0304mary@naver.com>
2025-04-29 13:51:11 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // 루트 디렉토리 설정
-  publicDir: 'public',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    rollupOptions: {
-      input: path.resolve(__dirname, 'index.html'), // index.html 위치 명시
-    },
   },
 });
